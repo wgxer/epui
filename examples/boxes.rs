19e8cbeedce6248418a00b732720ef6a52a9cf9f
wgxer <123264539+wgxer@users.noreply.github.com>
2023-11-26 17:03:33 +0300

    Made boxes example move boxes automatically

--- examples/boxes.rs
+++ examples/boxes.rs
@@ -1,13 +1,14 @@
 use bevy::{
-    prelude::{App, Color, Commands, Startup},
+    prelude::{App, Color, Commands, Query, Startup, Update, With},
     DefaultPlugins,
 };
 
-use epui::{prelude::*, property::state::hover::HoverEffect};
+use epui::prelude::*;
 
 fn main() {
     App::new()
         .add_systems(Startup, setup)
+        .add_systems(Update, move_boxes)
         .add_plugins(DefaultPlugins)
         .add_plugins(UiPlugin)
         .run();
@@ -20,20 +21,32 @@ fn setup(mut commands: Commands) {
 
     for x in 0..50 {
         for y in 0..50 {
-            bundles.push((
-                UiBoxBundle {
-                    color: ColoredElement::new(Color::RED),
+            bundles.push((UiBoxBundle {
+                color: ColoredElement::new(Color::RED),
 
-                    position: Position::new(x * 20, y * 20),
-                    size: Size::new(18, 18),
+                position: Position::new(x * 20, y * 20),
+                size: Size::new(18, 18),
 
-                    ..Default::default()
-                },
-                HoverEffect::new(ColoredElement::new(Color::DARK_GREEN)),
-                AABBCollisionBundle::default(),
-            ));
+                ..Default::default()
+            },));
         }
     }
 
     commands.spawn_batch(bundles);
+}
+
+fn move_boxes(mut boxes: Query<&mut Position, With<UiBox>>) {
+    for mut pos in boxes.iter_mut() {
+        if pos.x % 10 == 9 {
+            pos.x -= 9;
+        } else {
+            pos.x += 1;
+        }
+
+        if pos.y % 10 == 9 {
+            pos.y -= 9;
+        } else {
+            pos.y += 1;
+        }
+    }
 }
